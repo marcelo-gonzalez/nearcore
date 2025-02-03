55a3f5f5811b53039e43488e72fdedd5f558087f
Marcelo Diop-Gonzalez <marcelo827@gmail.com>
2025-02-03 13:34:23 -0500

    add pytest

---
+++ pytest/tests/sanity/parts.py
@@ -0,0 +1,24 @@
+import sys, time, base58, random
+import pathlib
+
+sys.path.append(str(pathlib.Path(__file__).resolve().parents[2] / 'lib'))
+
+from cluster import start_cluster
+from configured_logger import logger
+from transaction import sign_payment_tx
+import utils
+
+nodes = start_cluster(
+    num_nodes=1,
+    num_observers=0,
+    num_shards=4,
+    config=None,
+    extra_state_dumper=True,
+    genesis_config_changes=[["epoch_length", 50]],
+    client_config_changes={}
+)
+
+for height, hash in utils.poll_blocks(nodes[0], timeout=9999999999):
+    print(height)
+    if height > 700:
+        break
