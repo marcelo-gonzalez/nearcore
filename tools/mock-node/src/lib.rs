e1882059187ba3b1054a4ff9e2da569455b72ca2
Marcelo Diop-Gonzalez <marcelo827@gmail.com>
2025-02-04 16:33:37 -0500

    mock-node: Don't time out on receiving messages
    There's not a big downside to just waiting, and in the case where
    a node hasn't sent a message for a while, this probably does indicate
    something is wrong that we should investigate, but we don't get a lot
    out of the test just refusing to continue.

--- tools/mock-node/src/lib.rs
+++ tools/mock-node/src/lib.rs
@@ -481,7 +481,7 @@ impl MockNode {
             network_start_height,
             shard_layout.shard_ids().collect(),
             archival,
-            30 * near_time::Duration::SECOND,
+            None,
             handshake_protocol_version,
         )
         .await?;
